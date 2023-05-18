2d981ded072c52cd048fedeb4cbf069802efc1e5
PadTrick <my_mak@web.de>
2023-05-18 04:43:31 +0200

    kernel 6.3 build fix merge

--- git/Install_all_nvidia.sh
+++ git/Install_all_nvidia.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
 echo "creating dir $HOME/git and cloning all packages into it"
 mkdir $HOME/git
-echo "copy xone-dkms-git-6.3-kernel to $HOME/git"
-cp -a xone-dkms-git-6.3-kernel $HOME/git
+echo "xone-dkms-git to $HOME/git"
+cp -a xone-dkms-git $HOME/git
 cd $HOME/git
+clear
 echo "git clone ttf-kochi-substitute"
 git clone https://aur.archlinux.org/ttf-kochi-substitute.git
 echo "git clone nvidia-525xx-utils"
@@ -12,8 +13,12 @@ echo "git clone lib32-nvidia-525xx-utils"
 git clone https://aur.archlinux.org/lib32-nvidia-525xx-utils.git
 echo "git clone nvidia-525xx-settings.git"
 git clone https://aur.archlinux.org/nvidia-525xx-settings.git
-echo "Installing all packages"
-cd xone-dkms-git-6.3-kernel && makepkg -si
+clear
+echo "Installing Chaotix AUR packages"
+sudo pacman -S downgrade protontricks protonup-qt xone-dongle-firmware yay visual-studio-code-bin
+clear
+echo "Installing AUR and Custom packages"
+cd xone-dkms-git && makepkg -si
 cd $HOME/git
 cd ttf-kochi-substitute && makepkg -si
 cd $HOME/git
