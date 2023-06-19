c9d2f676b618ea386f105d4488ae5fe793878131
PadTrick <my_mak@web.de>
2023-06-19 16:06:22 +0200

    made opera media fix portable

--- Opera_Browser_No_Video_Playback_Fix/Copy_Media_File.sh
+++ Opera_Browser_No_Video_Playback_Fix/Copy_Media_File.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
+WORKING_DIR=$(pwd)
 sudo mkdir /usr/lib/opera/lib_extra
-sudo cp $HOME/_HDD/Linux/Opera_Browser_No_Video_Playback_Fix/usr/lib/opera/lib_extra/libffmpeg.so /usr/lib/opera/lib_extra/libffmpeg.so
+sudo cp $WORKING_DIR/usr/lib/opera/lib_extra/libffmpeg.so /usr/lib/opera/lib_extra/libffmpeg.so
